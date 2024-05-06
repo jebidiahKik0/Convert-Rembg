9a3ec1aa0109b8615fd65e921c022f3953c0c1d5
JLorio <jlorio@mailfence.com>
2024-05-06 04:32:26 -0400

    first commit

---
+++ remBG.py
@@ -0,0 +1,37 @@
+import io
+import os
+from PIL import Image
+from rembg import remove
+
+def convert_and_remove_bg(input_folder, output_folder):
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Loop through all files in the input folder
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".jpg"):  # Check if the file is a JPG
+            # Full path of the current file
+            file_path = os.path.join(input_folder, filename)
+            # Load the image
+            with open(file_path, 'rb') as file:
+                img_data = file.read()
+                img_no_bg = remove(img_data)  # Remove background
+
+            # Convert image to PNG
+            img = Image.open(io.BytesIO(img_no_bg))
+            # Creating PNG filename by replacing jpg with png
+            png_filename = filename[:-4] + '.png'
+            # Full path for the output file
+            output_file_path = os.path.join(output_folder, png_filename)
+            # Save the PNG image
+            img.save(output_file_path, "PNG")
+
+    print("Conversion and background removal completed.")
+
+# Specify the input and output folders
+input_folder = r"C:\Users\kik0\Pictures\JPG Images"
+output_folder = r"C:\Users\kik0\Pictures\Characters"
+
+# Call the function
+convert_and_remove_bg(input_folder, output_folder)
---
+++ webp2jpg.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import os
+
+def convert_webp_to_jpg(source_folder, output_folder):
+    # Create the output folder if it does not exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # List all files in the source folder
+    for filename in os.listdir(source_folder):
+        if filename.endswith('.webp'):
+            # Construct the full file path
+            webp_path = os.path.join(source_folder, filename)
+            # Open the .webp image file
+            with Image.open(webp_path) as img:
+                # Define the output file path, change extension to .jpg
+                jpg_path = os.path.join(output_folder, os.path.splitext(filename)[0] + '.jpg')
+                # Convert the image to RGB mode and save it as a .jpg
+                img.convert('RGB').save(jpg_path, 'jpeg')
+
+    print(f"All .webp images from {source_folder} have been converted to .jpg in {output_folder}")
+
+# Example usage
+source_folder = r"C:\Users\kik0\Pictures\Webp DnD Images"
+output_folder = r"C:\Users\kik0\Pictures\JPG Images"
+convert_webp_to_jpg(source_folder, output_folder)
